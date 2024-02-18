b7a93620cbf6fab9db7f32a4049bf219efc702af
Guille <51679006+GuiSchet@users.noreply.github.com>
2024-02-18 21:03:37 +0000

    clase 1

--- 01 - introduccion typescript/app.js
+++ 01 - introduccion typescript/app.js
@@ -1,12 +1,12 @@
-// las variables en typescript tienen que crearse junto con el tipo de dato asignado. "tipeada"
+// Las variables en typescript tienen que crearse junto con el tipo de dato asignado. "tipeada"
 var cadena = 'esto es una cadena de texto';
-// no va a dejar pisar el tipo de datos;
+// No va a dejar pisar el tipo de datos;
 //cadena = 23; va a dar error.
 var edad = 24;
 var booleano = true;
 console.log(cadena);
-//datos estructurados:
-// los parametros son de tipo dato nuumber, pero tb defino q si o si la funcion devuelve un dato numerico si o si.
+// Datos estructurados:
+// Los parametros son de tipo dato nuumber, pero tb defino q si o si la funcion devuelve un dato numerico si o si.
 function sumar(a, b) {
     return a + b;
 }
@@ -14,3 +14,8 @@ console.log(sumar(15, 10));
 function concatenarString(a, b) {
     return a + b;
 }
+// Uniontype
+// es un operador para poder combinar tipos de datos.
+var valorTipo;
+valorTipo = 'hola';
+valorTipo = 22;
--- 01 - introduccion typescript/app.ts
+++ 01 - introduccion typescript/app.ts
@@ -1,7 +1,7 @@
-// las variables en typescript tienen que crearse junto con el tipo de dato asignado. "tipeada"
+// Las variables en typescript tienen que crearse junto con el tipo de dato asignado. "tipeada"
 let cadena: string = 'esto es una cadena de texto';
 
-// no va a dejar pisar el tipo de datos;
+// No va a dejar pisar el tipo de datos;
 //cadena = 23; va a dar error.
 
 let edad: number = 24;
@@ -10,8 +10,8 @@ let booleano: boolean = true;
 
 console.log(cadena);
 
-//datos estructurados:
-// los parametros son de tipo dato nuumber, pero tb defino q si o si la funcion devuelve un dato numerico si o si.
+// Datos estructurados:
+// Los parametros son de tipo dato nuumber, pero tb defino q si o si la funcion devuelve un dato numerico si o si.
 function sumar(a: number, b: number): number {
     return a + b;
 
@@ -22,3 +22,12 @@ console.log(sumar(15, 10));
 function concatenarString (a: string, b: string): string {
     return a + b;
 }
+
+// Uniontype
+// es un operador para poder combinar tipos de datos.
+
+let valorTipo: string | number | boolean;
+
+valorTipo = 'hola';
+valorTipo = 22;
+
