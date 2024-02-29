915eb67ef322ff86229e1f014a8fcad8ed996dfc
Guille <51679006+GuiSchet@users.noreply.github.com>
2024-02-29 00:04:47 +0000

    clase 3

--- 03_POO/app.js
+++ 03_POO/app.js
@@ -1,4 +1,15 @@
-//ENUM - dato estructurado - enumeracion
+// ENUM - dato estructurado - enumeracion
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var Day;
 (function (Day) {
     Day[Day["Monday"] = 0] = "Monday";
@@ -10,17 +21,105 @@ var Day;
     Day[Day["Sunday"] = 6] = "Sunday";
 })(Day || (Day = {}));
 var today = Day.Monday;
-switch (today) {
-    case Day.Monday:
-        console.log('Hoy es lunes');
-        break;
-    case Day.Tuesday:
-        console.log('Hoy es martes');
-        break;
-    case Day.Wednesday:
-        console.log('Hoy es miercoles');
-        break;
-    case Day.Thursday:
-        console.log('Hoy es jueves');
-        break;
+console.log(Day[0]);
+// objeto literal
+var PersonaUno = {
+    nombre: 'Cecilia',
+    edad: 24,
+    profesion: 'programadora'
+};
+var PersonaDos = {
+    nombre: 'maria',
+    edad: 33,
+    profesion: 'ingeniera'
+};
+console.log(PersonaUno);
+console.log(PersonaDos);
+var perro = {
+    tipo: 'canino',
+    nombre: 'ringo'
+};
+console.log(perro);
+function mostrarPerro(perro) {
+    console.log(perro.nombre);
 }
+mostrarPerro(perro);
+// Representar los estados de una tarea.
+var EstadoTareas;
+(function (EstadoTareas) {
+    EstadoTareas["Pendiente"] = "Pendiente";
+    EstadoTareas["EnProgreso"] = "En progreso";
+    EstadoTareas["Completado"] = "Completado";
+})(EstadoTareas || (EstadoTareas = {}));
+;
+;
+// Funcion para crear nuevas tareas
+function crearTarea(id, descripcion) {
+    return {
+        id: id,
+        descripcion: descripcion,
+        estado: EstadoTareas.Pendiente
+    };
+}
+;
+// Funcion se encargue de actualizar el estado de una tarea
+// ... clonar array, objeto.
+function estadoTarea(tarea, nuevoEstado) {
+    return __assign(__assign({}, tarea), { estado: nuevoEstado });
+}
+// Funcion par imprimir informacion sobre la tarea
+function infoTareas(tarea) {
+    console.log("Tarea ".concat(tarea.id, ": ").concat(tarea.descripcion, " - Estado: ").concat(tarea.estado));
+}
+// Uso de las funciones
+var tareaUno = crearTarea(1, 'Pintar la casa');
+var tareaDos = crearTarea(2, 'Estudiar typescript');
+infoTareas(tareaUno);
+tareaUno = estadoTarea(tareaUno, EstadoTareas.EnProgreso);
+infoTareas(tareaUno);
+;
+;
+;
+var empleadoJuan = {
+    nombre: 'Juan',
+    puesto: 'Desarrollador',
+    departamento: 'IT',
+    ubicacion: 'oficina 122',
+    seguroMedico: true,
+    diasVacaciones: 201
+};
+var empleadaSofia = {
+    nombre: 'sofia',
+    puesto: 'Desarrollador',
+    departamento: 'marketing',
+    ubicacion: 'oficina 123',
+    seguroMedico: false,
+    diasVacaciones: 5
+};
+console.log(empleadoJuan);
+// Programacion orientada a objetos. Clases.
+// plantilla bastante estructurada de un objeto.
+// el constructor es un metodo especial y es quien inicializa las propiedades q va a tener ell objeto cuando see implemente en la instancia real.
+var Persona = /** @class */ (function () {
+    function Persona(nombre, edad) {
+        nombre: nombre;
+        edad: edad;
+    }
+    Persona.prototype.saludar = function () {
+        console.log("Hola, soy ".concat(this.nombre, " y tengo ").concat(this.edad, " anios."));
+    };
+    return Persona;
+}());
+// saludar es un metodo de Persona.
+var usuario = new Persona('Sofia', 25);
+console.log(usuario);
+// otra clase.
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    Person.prototype.getName = function () {
+        return this.name;
+    };
+    return Person;
+}());
--- 03_POO/app.ts
+++ 03_POO/app.ts
@@ -1,28 +1,179 @@
-//ENUM - dato estructurado - enumeracion
+// ENUM - dato estructurado - enumeracion
 
 enum Day {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
+    Monday = 0,
+    Tuesday = 1,
+    Wednesday = 2,
+    Thursday = 3,
+    Friday = 4,
+    Saturday = 5,
+    Sunday = 6
 }
 
 let today: Day = Day.Monday;
 
-switch(today) {
-    case Day.Monday:
-        console.log('Hoy es lunes');
-        break;
-    case Day.Tuesday:
-        console.log('Hoy es martes');
-        break;
-    case Day.Wednesday:
-        console.log('Hoy es miercoles');
-        break;
-    case Day.Thursday:
-        console.log('Hoy es jueves');
-        break;        
+console.log(Day[0]);
+
+// Interface, para crear un esquema de objeto.
+// no existe como objeto.
+interface Persona1 {
+    nombre: string,
+    edad: number,
+    profesion: string
 }
+
+// objeto literal
+let PersonaUno: Persona1 = {
+    nombre: 'Cecilia',
+    edad: 24,
+    profesion: 'programadora'
+}
+
+let PersonaDos: Persona1 = {
+    nombre: 'maria',
+    edad: 33,
+    profesion: 'ingeniera'
+}
+
+console.log(PersonaUno);
+console.log(PersonaDos);
+
+// Extender una interface de otra.
+
+interface Animal {
+    tipo: string,
+}
+
+interface Mascota extends Animal {
+    nombre: string
+}
+
+let perro: Mascota = {
+    tipo: 'canino',
+    nombre: 'ringo'
+}
+
+console.log(perro);
+
+function mostrarPerro(perro: Mascota) {
+    console.log(perro.nombre);
+}
+
+mostrarPerro(perro);
+
+// Representar los estados de una tarea.
+
+enum EstadoTareas {
+    Pendiente = 'Pendiente',
+    EnProgreso = 'En progreso',
+    Completado = 'Completado'
+};
+
+interface Tareas {
+    id: number,
+    descripcion: string,
+    estado: EstadoTareas
+};
+
+// Funcion para crear nuevas tareas
+
+function crearTarea(id: number, descripcion: string): Tareas {
+    return {
+        id: id,
+        descripcion: descripcion,
+        estado: EstadoTareas.Pendiente
+    }    
+};
+
+// Funcion se encargue de actualizar el estado de una tarea
+// ... clonar array, objeto.
+function estadoTarea (tarea: Tareas, nuevoEstado: EstadoTareas): Tareas {
+    return {...tarea, estado: nuevoEstado}
+}
+
+// Funcion par imprimir informacion sobre la tarea
+function infoTareas(tarea: Tareas) {
+    console.log(`Tarea ${tarea.id}: ${tarea.descripcion} - Estado: ${tarea.estado}`);
+}
+
+// Uso de las funciones
+
+let tareaUno = crearTarea(1, 'Pintar la casa');
+let tareaDos = crearTarea(2, 'Estudiar typescript');
+infoTareas(tareaUno);
+tareaUno = estadoTarea(tareaUno, EstadoTareas.EnProgreso);
+infoTareas(tareaUno);
+
+// Mezclar interface
+
+interface Empleado {
+    nombre: string,
+    puesto: string
+};
+
+interface Departamento {
+    departamento: string,
+    ubicacion: string
+};
+
+interface Beneficios {
+    seguroMedico: boolean,
+    diasVacaciones: number
+};
+
+// interseccion de interfaces.
+
+type InformacionEmpleado = Empleado & Departamento & Beneficios;
+
+let empleadoJuan: InformacionEmpleado = {
+    nombre: 'Juan',
+    puesto: 'Desarrollador',
+    departamento: 'IT',
+    ubicacion: 'oficina 122',
+    seguroMedico: true,
+    diasVacaciones: 201
+}
+
+let empleadaSofia: InformacionEmpleado = {
+    nombre: 'sofia',
+    puesto: 'Desarrollador',
+    departamento: 'marketing',
+    ubicacion: 'oficina 123',
+    seguroMedico: false,
+    diasVacaciones: 5
+}
+
+console.log(empleadoJuan);
+
+// Programacion orientada a objetos. Clases.
+// plantilla bastante estructurada de un objeto.
+
+// el constructor es un metodo especial y es quien inicializa las propiedades q va a tener ell objeto cuando see implemente en la instancia real.
+class Persona {
+    nombre: string;
+    edad: number;
+    constructor(nombre: string, edad: number) {
+        nombre: nombre;
+        edad: edad;
+    }
+    saludar() {
+        console.log(`Hola, soy ${this.nombre} y tengo ${this.edad} anios.`)
+    }
+}
+// saludar es un metodo de Persona.
+
+const usuario = new Persona('Sofia', 25);
+console.log(usuario);
+
+// otra clase.
+class Person {
+    name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+  
+    getName(): string {
+      return this.name;
+    }
+  }
+
